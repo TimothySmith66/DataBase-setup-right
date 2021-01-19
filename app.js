9b51f05c220aed82963d5475742c5b1ab0753d5f
Timothy Smith <timothytsmith8@gmail.com>
2021-01-18 23:17:06 -0800

    got connection set up and created a .sql file.

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+
+
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "localhost2"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    createProduct();
+  });
+  
+  
 //const inquirerr = () =>
 
-inquirer
-    .prompt([
 
-
-
-
-
-        
-    ])
+// inquirer
+//   .prompt([
+//     {
+//         type: 'input',
+//         message: 'What is the title of your project?',
+//         name: 'name',
+//     },
+//   ])
+//   .then(answers => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch(error => {
+//     if(error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else when wrong
+//     }
+//   });
