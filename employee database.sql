4f8d39bfdda55fd91fc2d9c405e36fef74adceea
Timothy Smith <timothytsmith8@gmail.com>
2021-01-18 23:23:55 -0800

    Created three tables

---
+++ employee database.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE employer_db;
+ 
+ USE employer_db;
+ 
+ CREATE table employee(
+ id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+ first_name VARCHAR(50) NOT NULL,
+ last_name VARCHAR (50) NOT NULL, 
+ role_id INT NOT NULL,
+ manager_id INT,
+ PRIMARY KEY (id)
+ );
+ CREATE TABLE department(
+ id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+ name varchar(50) NOT NULL,
+ PRIMARY KEY(id)
+ );
+ 
+ CREATE TABLE role( 
+ id INT NOT NULL AUTO_INCREMENT,
+ salary DECIMAL NOT NULL,
+ title VARCHAR (30) NOT NULL,
+ department_id INT NOT NULL,
+ PRIMARY KEY (id)
+ );
